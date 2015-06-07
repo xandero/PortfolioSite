972aeeea543fdc77ab6d3ed67866887aa228be98
xandero <alexander.ohan@gmail.com>
2015-06-07 18:20:36 +1000

    fiddling with portfolio display

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -1,8 +1,8 @@
 <?php
 
 	// Contact
-	$to = 'web-master72@yandex.ru';
-	$subject = 'Subject here...';
+	$to = 'alexander.ohan@gmail.com';
+	$subject = 'Portfolio site new contact';
 
 	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_message'])){
 		$name    = $_POST['c_name'];
@@ -11,13 +11,13 @@
 
 		if (mail($to, $subject, $message, $from)) {
 			$result = array(
-				'message' => 'Thanks for contacting us!',
+				'message' => "Thanks for contacting me! You'll hear from me soon.",
 				'sendstatus' => 1
 				);
 			echo json_encode($result);
 		} else {
 			$result = array(
-				'message' => 'Sorry, something is wrong',
+				'message' => 'Sorry, something went wrong',
 				'sendstatus' => 1
 				);
 			echo json_encode($result);
